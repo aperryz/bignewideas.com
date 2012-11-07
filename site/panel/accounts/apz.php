a739fa186ff3b42c0a06d7dfa4ba435791270165
Jeffrey Lo <hello@jeffreylo.cc>
2012-11-07 14:18:34 -0500

    SHA1 Encryption
    Signed-off-by: Jeffrey Lo <hello@jeffreylo.cc>

--- site/panel/accounts/apz.php
+++ site/panel/accounts/apz.php
@@ -4,8 +4,9 @@
 # it's highly recommended to use md5 or sha1 encryption 
 # for your passwords. read more about encryption in the
 # docs: http://getkirby.com/docs/panel/accounts
+# 9:2P%7?=9nb3i6)]g(48kQJL^T7{sR
 
-username: jeffreylo
-password: 841da23beb81b65cfbb853b9a3e9cc6b659cbcd1
+username: apz
+password: a609536e1f455834a98df3bdc7bb02c6057d06e4
 encrypt:  sha1
 language: en
--- site/panel/accounts/slavkin.php
+++ site/panel/accounts/slavkin.php
@@ -4,8 +4,9 @@
 # it's highly recommended to use md5 or sha1 encryption 
 # for your passwords. read more about encryption in the
 # docs: http://getkirby.com/docs/panel/accounts
+# .43KfoZ9w?JR8P967:M/T2]%{c}[8F
 
-username: jeffreylo
-password: 841da23beb81b65cfbb853b9a3e9cc6b659cbcd1
+username: slavkin
+password: 3c33bed1fdda2ddc0655bb0fb2154b1f7ef08552
 encrypt:  sha1
 language: en
